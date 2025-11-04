a17ec191844ba1e4f3782e58081e9e91f6eb3e3e
Sabyasachi Sahoo <sabyasachi3098@gmail.com>
2025-11-04 16:59:38 +0530

    1st commit to feature branch

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -28,7 +28,7 @@ module "eks" {
     two = {
       name = "node-group-2"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t3.medium"]
 
       min_size     = 1
       max_size     = 2
--- terraform/version.tf
+++ terraform/version.tf
@@ -27,12 +27,12 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofile3098"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = "ap-south-1"
   }
 
-  required_version = "~> 1.6.3"
+  required_version = "~> 1.12.1"
 }
 ##
 ##
